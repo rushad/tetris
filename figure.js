05167900c0c6e9ac55f37e675bb7f71066e132e2
rushad <rushad.nab@gmail.com>
2014-10-06 19:20:25 +0400

    game class

---
+++ figure.js
@@ -0,0 +1,48 @@
+﻿function Figure(game)
+{
+	var figure =
+	{
+		x: 3,
+		y: 0,
+		color: "blue",
+		drawWithColor: function(color)
+		{
+			game.ctx.fillStyle = color;
+			game.ctx.fillRect(this.x * game.cellWidth + 1, this.y * game.cellHeight + 1, game.cellWidth - 2, game.cellHeight - 2);
+			game.ctx.fillRect((this.x + 1) * game.cellWidth + 1, this.y * game.cellHeight + 1, game.cellWidth - 2, game.cellHeight - 2);
+			game.ctx.fillRect((this.x + 2) * game.cellWidth + 1, this.y * game.cellHeight + 1, game.cellWidth - 2, game.cellHeight - 2);
+			game.ctx.fillRect((this.x + 3) * game.cellWidth + 1, this.y * game.cellHeight + 1, game.cellWidth - 2, game.cellHeight - 2);
+		},
+		
+		clear: function()
+		{
+			this.drawWithColor(game.backgroundColor);
+		},
+		
+		draw: function()
+		{
+			this.drawWithColor(this.color);
+		},
+		
+		lower: function()
+		{
+			this.clear();
+			this.y++;
+			this.draw();
+		},
+		
+		start: function()
+		{
+			var a = this;
+			setInterval(a.lower, 1000);
+		}
+	};
+	
+	figure.draw();
+	figure.lower();
+	figure.lower();
+
+	figure.start();
+	
+	return figure;
+}
---
+++ tetris.js
@@ -0,0 +1,76 @@
+﻿function Tetris(canvasId)
+{
+	var WIDTH_IN_CELLS = 10;
+	var HEIGHT_IN_CELLS = 20;
+	var CELL_WIDTH = 25;
+	var CELL_HEIGHT = 25;
+	var BACKGROUND_COLOR = "black";
+	var GRID_COLOR = "gray";
+	
+	var canvasElement = document.getElementById(canvasId);
+	if (!canvasElement || !canvasElement.getContext)
+	{
+		console.log("No canvas with id=" + canvasId);
+		return;
+	}
+
+	var game = 
+	{
+		widthInCells: WIDTH_IN_CELLS,
+		heightInCells: HEIGHT_IN_CELLS,
+		cellWidth: CELL_WIDTH,
+		cellHeight: CELL_HEIGHT,
+		canvasWidth: WIDTH_IN_CELLS * CELL_WIDTH,
+		canvasHeight: HEIGHT_IN_CELLS * CELL_HEIGHT,
+		backgroundColor: BACKGROUND_COLOR,
+		gridColor: GRID_COLOR,
+		ctx: canvasElement.getContext("2d"),
+		loop: null,
+		figure: null,
+		
+		drawGameBoard: function() 
+		{
+			this.ctx.fillStyle = this.backgroundColor;
+			this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+			this.ctx.strokeStyle = this.gridColor;
+			this.ctx.lineWidth = 1;
+			this.ctx.translate(0.5, 0.5);
+			for(var x = 0; x <= this.widthInCells; ++x)
+			{
+				this.ctx.moveTo(x * this.cellWidth, 0);
+				this.ctx.lineTo(x * this.cellWidth, this.canvasHeight);
+				this.ctx.stroke();
+			}
+			for(var y = 0; y <= this.heightInCells; ++y)
+			{
+				this.ctx.moveTo(0, y * this.cellHeight);
+				this.ctx.lineTo(this.canvasWidth, y * this.cellHeight);
+				this.ctx.stroke();
+			}		
+		}
+	};
+	
+	canvasElement.width = game.canvasWidth;
+	canvasElement.height = game.canvasHeight;
+	
+	game.drawGameBoard();
+
+	game.figure = new Figure(game);
+	
+/*	
+	return 
+	{
+		dimWidth: 10,
+		dimHeight: 20,
+		cellWidth: 30,
+		cellHeight: 30,
+	var canvasWidth = dimWidth * cellWidth + 1;
+	var canvasHeight = dimHeight * cellHeight + 1;
+	var curX = 0;
+	var curY = -1;
+	var curFigure = 0;
+	var canvas;
+	var ctx;
+	var loop = null;
+*/	
+}
